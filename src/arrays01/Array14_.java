70f93d10dc953ea112e410970d78093afe96cdcb
Juergen-J <yurk.jur@gmail.com>
2021-01-06 12:40:39 +0100

    L-23

--- src/arrays01/Array14_.java
+++ src/arrays01/Array14_.java
@@ -8,6 +8,11 @@ public class Array14_ {
         makeLast(new int[]{4, 5, 6});// → [0, 0, 0, 0, 0, 6]
         makeLast(new int[]{1, 2});// → [0, 0, 0, 2]
         makeLast(new int[]{3});// → [0, 3]
+
+        System.out.println("----------------#15");
+        System.out.println(start1(new int[]{1, 2, 3}, new int[]{1, 3}));// →2
+        System.out.println(start1(new int[]{7, 2, 3}, new int[]{1}));// →1
+        System.out.println(start1(new int[]{1, 2}, new int[]{0}));//1
     }
 
     public static int[] makeLast(int[] array) {
@@ -15,5 +20,16 @@ public class Array14_ {
         result[result.length - 1] = array[array.length - 1];
         PrintArray.printIntArray(result);
         return result;
+    }
+
+    public static int start1(int[] a, int[] b) {
+        int count = 0;
+        if (a[0] == 1) {
+            count++;
+        }
+        if (b[0] == 1) {
+            count++;
+        }
+        return count;
     }
 }
