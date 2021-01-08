b50b31a0545e815e57b327f8f43aec5ea407ed92
Juergen-J <yurk.jur@gmail.com>
2021-01-08 11:52:57 +0100

    L-25

--- src/arrays01/Array24_.java
+++ src/arrays01/Array24_.java
@@ -8,6 +8,11 @@ public class Array24_ {
         biggerTwo(new int[]{1, 2}, new int[]{3, 4});// → [3, 4]
         biggerTwo(new int[]{3, 4}, new int[]{1, 2});// → [3, 4]
         biggerTwo(new int[]{1, 1}, new int[]{1, 2});// → [1, 2]
+
+        System.out.println("----------------#24");
+        swapEnds(new int[]{1, 2, 3, 4});// → [4, 2, 3, 1]
+        swapEnds(new int[]{1, 2, 3});// → [3, 2, 1]
+        swapEnds(new int[]{8, 6, 7, 9, 5});// → [5, 6, 7, 9, 8]
     }
 
     public static int[] biggerTwo(int[] a, int[] b) {
@@ -16,7 +21,18 @@ public class Array24_ {
         PrintArray.printIntArray(result);
         return result;
     }
+
     public static int arraySum(int[] array) {
         return array[0] + array[1];
+    }
+
+    public static int[] swapEnds(int[] array) {
+        int[] result = new int[array.length];
+        result[0] = array[array.length - 1];
+        for (int i = 1; i < array.length - 1; i++) {
+            result[i] = array[i];
+        }
+        result[result.length - 1] = array[0];
+        return result;
     }
 }
---
+++ src/lesson25/Lesson25.java
@@ -0,0 +1,9 @@
+package lesson25;
+
+public class Lesson25 {
+    public static void main(String[] args) {
+        int a=5;
+        int[]arr= new int[10];
+        int [][] monthCalendar= new int[4][7];
+    }
+}
