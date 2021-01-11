1c5c7f3b0ae6c6b65cb2ab113b3442efa7e185ca
Juergen-J <yurk.jur@gmail.com>
2021-01-11 12:23:25 +0100

    L-26

--- src/arrays01/Array26_.java
+++ src/arrays01/Array26_.java
@@ -11,6 +11,11 @@ public class Array26_ {
         frontPiece(new int[]{1, 2, 3});// → [1, 2]
         frontPiece(new int[]{1, 2});// → [1, 2]
         frontPiece(new int[]{1});// → [1]
+
+        System.out.println("*******************#27");
+        front11(new int[]{1, 2, 3}, new int[]{7, 9, 8});// → [1, 7]
+        front11(new int[]{1}, new int[]{2});// → [1, 2]
+        front11(new int[]{1, 7}, new int[0]);// → [1]
     }
 
     public static int[] frontPiece(int[] array) {
@@ -21,6 +26,21 @@ public class Array26_ {
         } else {
             result[0] = array[0];
             result[1] = array[1];
+        }
+        PrintArray.printIntArray(result);
+        return result;
+    }
+
+    public static int[] front11(int[] a, int[] b) {
+        int length = a.length > 0 && b.length > 0 ? 2 : 1;
+        int[] result = new int[length];
+        if (length == 2) {
+            result[0] = a[0];
+            result[1] = b[0];
+        } else if (a.length > 0) {
+            result[0] = a[0];
+        } else {
+            result[0] = b[0];
         }
         PrintArray.printIntArray(result);
         return result;
