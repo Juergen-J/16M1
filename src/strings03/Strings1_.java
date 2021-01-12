fab7c6670b5364f98182fb934785b657c202ad1e
Juergen-J <yurk.jur@gmail.com>
2021-01-12 12:01:38 +0100

    L-27

---
+++ src/strings03/Strings1_.java
@@ -0,0 +1,16 @@
+package strings03;
+
+public class Strings1_ {
+    public static void main(String[] args) {
+        System.out.println("**************#1");
+        System.out.println(countYZ("fez day"));// → 2
+        System.out.println(countYZ("day fez"));// → 2
+        System.out.println(countYZ("day fyyyz"));// → 2
+    }
+
+    public static int countYZ(String str) {
+        int count = 0;
+
+        return count;
+    }
+}
