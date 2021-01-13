3c6895c7b22739f126861d6aeab5345b10da66b4
Juergen-J <yurk.jur@gmail.com>
2021-01-13 12:25:30 +0100

    L-28

--- src/lesson28/CalculationOfTiles.java
+++ src/lesson28/CalculationOfTiles.java
@@ -1,36 +1,30 @@
 package lesson28;
 
+import lesson20.Main;
+
+import java.util.Scanner;
+
 public class CalculationOfTiles {
     public static void main(String[] args) {
-//        Нам известно, что в час он кладёт примерно один квадратный метр плитки.//
-//                Его час стоит 27€ без НДС.
-//                Стандартная плитка 30 на 20 сантиметров.
-//                Сергей Иванович обратился к нам за помощью и вам надо написать для него программу.
-//                Которая будет принимать размеры стены/пола, стоимость плитки и выдавать:
-//
-//        Количество требуемых плиток, как целых, так и тех которые надо будет резать. Учтите,
-//        что плитку можно класть как вдоль, так и поперёк и только на месте мастер узнает, как именно её надо класть.
-//
-//        Стоимость материала
-//        Стоимость рабочего времени
-//        Итоговую ориентировочную стоимость
+        Scanner scaner = new Scanner(System.in);
+        final String ANSI_RESET = "\u001B[0m";
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+        final String ANSI_RED_BACKGROUND = "\u001B[41m";
 
         double aTile = 30;//cm
         double bTile = 20;//cm
-
-
-        double aZone = 200;//cm
-        double bZone = 4096;//cm
-        System.out.println(getZoneArea(aZone, bZone));//80000 cm^2
-
-        getQuantityTiles(aZone, bZone, aTile, bTile);
-
-
-        double tilesPrise;
-
-        getCostOfMaterial();
-        getCostOfTime();
-        getTotalCost();
+        System.out.println(ANSI_BLACK_BACKGROUND + ANSI_RED + "enter the width of the zone in centimeters..." + ANSI_RESET);
+        double aZone = scaner.nextDouble();//cm
+        System.out.println(ANSI_BLACK_BACKGROUND + ANSI_RED + "enter the length of the zone in centimeters..." + ANSI_RESET);
+        double bZone = scaner.nextDouble();//cm
+        System.out.println(ANSI_BLACK_BACKGROUND + ANSI_RED + "enter the cost of one tile (Euro, *,*)..." + ANSI_RESET);
+        double costOfTile = scaner.nextDouble();//Euro
+        double zoneArea = getZoneArea(aZone, bZone);//80000 cm^2
+        int tilesQuantity = getQuantityTiles(aZone, bZone, aTile, bTile);
+        System.out.println(ANSI_RED + getCostOfMaterial(tilesQuantity, costOfTile) + " Euro for Tiles" + ANSI_RESET);
+        System.out.println(ANSI_RED + getCostOfTime(zoneArea) + " Euro for work inc. Mwst." + ANSI_RESET);
+        System.out.println(ANSI_RED_BACKGROUND + "Total cost: " + getTotalCost(getCostOfTime(zoneArea), getCostOfMaterial(tilesQuantity, costOfTile)) + " Euro" + ANSI_RESET);
 
 
     }
@@ -39,17 +33,16 @@ public class CalculationOfTiles {
         return a * b;
     }
 
-    public static void getCostOfMaterial() {
-
+    public static double getCostOfMaterial(int quantity, double cost) {
+        //System.out.println("Cost of tiles: " + quantity * cost + " Euro ");
+        return roundMoney(quantity * cost);
     }
 
     public static int getQuantityTiles(double aZone, double bZone, double aTile, double bTile) {
         double temp = aZone / aTile * bZone / bTile;
-        System.out.println(temp);
         double temp2 = aZone / bTile * bZone / aTile;
-        System.out.println(temp2);
         int result = getRoundQuantity(temp, temp2);
-        System.out.println(result);
+        System.out.println(result + " Tiles");
         return result;
     }
 
@@ -59,11 +52,19 @@ public class CalculationOfTiles {
         return temp2 < temp ? temp2 + 1 : temp2;
     }
 
-    public static void getCostOfTime() {
-
+    public static double getCostOfTime(double area) {
+        double preis = (area / 10000) * 27;
+        //System.out.println("cost of work: " + preis + "Euro  +19% Mehrwertsteuer:" + preis * 0.19 + " Euro");
+        return roundMoney(preis + preis * 0.19);
     }
 
-    public static void getTotalCost() {
+    public static double roundMoney(double money) {
+        int temp = (int) (money * 100);
+        double temp2 = temp;
+        return temp2 / 100;
+    }
 
+    public static double getTotalCost(double workCost, double tilesCost) {
+        return roundMoney(workCost + tilesCost);
     }
 }
--- src/library/MyColor.java
+++ src/library/MyColor.java
@@ -10,12 +10,12 @@ public class MyColor {
         final String ANSI_BLACK = "\u001B[30m";
         final String ANSI_RED = "\u001B[31m";
         final String ANSI_GREEN = "\u001B[32m";
-
+        final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+        final String ANSI_RED_BACKGROUND = "\u001B[41m";
 
         //System.out.println(ANSI_GREEN + "Hello World" + ANSI_RESET);
 
-        final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-        final String ANSI_RED_BACKGROUND = "\u001B[41m";
+
       //  System.out.println(ANSI_RED_BACKGROUND + "HELLO" + ANSI_RESET);
     }
 }
