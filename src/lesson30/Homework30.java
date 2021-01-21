157e539137642a9188403398a93d25ebd6ab6fcc
Juergen-J <yurk.jur@gmail.com>
2021-01-21 12:27:24 +0100

    HW30

--- src/lesson30/Homework30.java
+++ src/lesson30/Homework30.java
@@ -1,16 +1,19 @@
 package lesson30;
 
+import library.PrintArray;
+
 public class Homework30 {
     public static void main(String[] args) {
         createRandomDoubleArray(10);
 
-        System.out.println(library.Random.getDoubleRandom(2, 8));
     }
 
     public static double[] createRandomDoubleArray(int length) {
         double[] result = new double[length];
-
-
+        for (int i = 0; i < result.length; i++) {
+            result[i] = library.Random.getDoubleRandom(1, 100);
+        }
+        PrintArray.printDoubleArray(result);
         return result;
     }
 
