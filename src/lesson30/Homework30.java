08738c019409c70dea09be7cf68ce2cdca0e8d88
Juergen-J <yurk.jur@gmail.com>
2021-01-21 13:54:45 +0100

    HW30

--- src/lesson30/Homework30.java
+++ src/lesson30/Homework30.java
@@ -1,6 +1,5 @@
 package lesson30;
 
-import library.PrintArray;
 
 public class Homework30 {
     public static void main(String[] args) {
@@ -8,7 +7,8 @@ public class Homework30 {
         bubbleSortDoubleArray(array);
         addElementDoubleArray(array, 5, 3.14);
         deleteElementDoubleArray(array, 3);
-
+        updateElementDoubleArray(array, 2, 7.77);
+        fillArray(array);
     }
 
     public static double[] createRandomDoubleArray(int length) {
@@ -44,7 +44,6 @@ public class Homework30 {
         for (int i = index + 1; i < output.length; i++) {
             output[i] = array[i - 1];
         }
-        PrintArray.printDoubleArray(output);
         return output;
     }
 
@@ -57,6 +56,35 @@ public class Homework30 {
             output[i - 1] = array[i];
         }
         return output;
+    }
+
+    public static double[] updateElementDoubleArray(double[] array, int index, double element) {
+        double[] output = array;
+        for (int i = 0; i < output.length; i++) {
+            if (i == index) {
+                output[i] = element;
+            }
+        }
+        printColorDoubleArray(output);
+        return output;
+    }
+
+    public static void printColorDoubleArray(double[] array) {
+        final String ANSI_RESET = "\u001B[0m";
+        final String ANSI_BLACK = "\u001B[30m";
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_GREEN = "\u001B[32m";
+
+        System.out.print(ANSI_RED + "[" + ANSI_RESET);
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.print(ANSI_GREEN + array[i] + ANSI_BLACK + ", ");
+        }
+        System.out.println(ANSI_GREEN + array[array.length - 1] + ANSI_RED + "]");
+    }
+
+    private static double[] fillArray(double[] array) {
+        double[] result = createRandomDoubleArray(array.length);
+        return result;
     }
 
 }
